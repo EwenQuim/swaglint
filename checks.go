febda44ece0f0a0da53e054dd05c27c2e72320cd
EwenQuim <ewen.quimerch@gmail.com>
2022-08-08 18:47:27 +0200

    feat: queryParam for stdhttp

--- checks.go
+++ checks.go
@@ -3,6 +3,10 @@ package main
 import (
 	"go/ast"
 	"strings"
+
+	"github.com/EwenQuim/swaglint/utils"
+	"golang.org/x/exp/slices"
+	"golang.org/x/tools/go/analysis"
 )
 
 func checkDocExists(funcDecl *ast.FuncDecl) bool {
@@ -31,4 +35,20 @@ func checkMissingTags(funcDecl *ast.FuncDecl) []string {
 		}
 	}
 	return missing
+}
+
+func checkWrongQueryParams(pass *analysis.Pass, funcDecl *ast.FuncDecl) {
+	paramsFromCode := utils.GetParamsFromFunctionBody(funcDecl.Body)
+	paramsFromDocs := utils.GetParamsFromDoc(funcDecl.Doc)
+
+	for _, param := range paramsFromCode {
+		if !slices.Contains(paramsFromDocs, param) {
+			pass.Reportf(funcDecl.Pos(), "%s is in code but not in docs\n", param)
+		}
+	}
+	for _, param := range paramsFromDocs {
+		if !slices.Contains(paramsFromCode, param) {
+			pass.Reportf(funcDecl.Pos(), "%s is in docs but not in code\n", param)
+		}
+	}
 }
--- testdata/src/stdhttp/positives.go
+++ testdata/src/stdhttp/positives.go
@@ -1,6 +1,7 @@
 package stdhttp
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -27,5 +28,21 @@ func _(w http.ResponseWriter, r *http.Request) { // want "should have the follow
 // @Summary Hello, world!
 // @Router /hello [get]
 func _(w http.ResponseWriter, r *http.Request) { // want "should have the following tags: @Tags"
+	w.Write([]byte("Hello, world!"))
+}
+
+// @Summary Hello, world!
+// @Tags user
+// @Router /hello [get]
+func _(w http.ResponseWriter, r *http.Request) { // want "clientID is in code but not in docs"
+	clientID := r.FormValue("clientID")
+	json.NewEncoder(w).Encode(clientID)
+}
+
+// @Summary Hello, world!
+// @Tags user
+// @Param clientID query string true "Client ID"
+// @Router /hello [get]
+func _(w http.ResponseWriter, r *http.Request) { // want "clientID is in docs but not in code"
 	w.Write([]byte("Hello, world!"))
 }
